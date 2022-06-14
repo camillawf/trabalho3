e0a9a9bf1a14ac20842bcd196874f28bdde9891c
camillawf <102559170+camillawf@users.noreply.github.com>
2022-06-13 21:19:59 -0300

    Delete script[1].js

--- script[1].js
+++
@@ -1,9 +0,0 @@
-function chamar_api(){
-  fetch('https://nekos.best/api/v2/neko')
-  .then(response => response.json())
-  .then(data => exibir_imagem(data))}
-
- function imagem(data){
-        console.log(data)
-}
-
